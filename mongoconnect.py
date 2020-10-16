7e0ed760375efa199b1cc8e6e0581f7c019d2cd7
penguinofwinter <winterbirdwinterbird@gmail.com>
2020-10-15 22:14:29 -0400

    connect function and read function

--- mongoconnect.py
+++ mongoconnect.py
@@ -1,12 +1,35 @@
+import pandas as pd
 from pymongo import MongoClient
 
-# Create a mongo connection
-client = MongoClient("mongodb+srv://mongo:mongo2247@spotifysuperdata.tuo6e.gcp.mongodb.net/test")
 
-# Have a look at all the databases in the client connection
-print(client.list_database_names())
+def _connect_mongo(host, port, username, password, db):
+    """ A util for making a connection to mongo """
 
-# Have a look at all the collection names in all the databases in client connection
-for item in client.list_database_names():
-    tempDB = client[item]
-    print(f'Database: {item:<20}, Collections: {tempDB.list_collection_names()}')
+    if username and password:
+        mongo_uri = 'mongodb://%s:%s@%s:%s/%s' % (username, password, host, port, db)
+        conn = MongoClient(mongo_uri)
+    else:
+        conn = MongoClient(host, port)
+
+
+    return conn[db]
+
+
+def read_mongo(db, collection, query={}, host='localhost', port=27017, username=None, password=None, no_id=True):
+    """ Read from Mongo and Store into DataFrame """
+
+    # Connect to MongoDB
+    db = _connect_mongo(host=host, port=port, username=username, password=password, db=db)
+
+    # Make a query to the specific DB and Collection
+    cursor = db[collection].find(query)
+
+    # Expand the cursor and construct the DataFrame
+    df =  pd.DataFrame(list(cursor))
+
+    # Delete the _id
+    if no_id:
+        del df['_id']
+
+    return df
+    
