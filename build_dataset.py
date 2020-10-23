3fd4bd2e1ad18e3336209acad3fb81ad5b2e8319
ryanrwillis <ryan.willis@hotmail.ca>
2020-10-22 23:31:09 -0400

    added dataset building

--- build_dataset.py
+++ build_dataset.py
@@ -1,9 +1,18 @@
 from search import search_playlist
 from playlist import Playlist
 from azlyrics import azlyrics
+from config import mongo_connect_uri
+from globals import spotify_api_base
+import pymongo
+import requests
+import json
 import re
 
-def build_dataset(genre_list, token, playlist_per_genre=1):
+def build_dataset(genre_list, token, playlist_per_genre=8):
+    client = pymongo.MongoClient(mongo_connect_uri)
+    db = client.songs
+    collection = db['songs']
+
     playlist_index = []
     for genre in genre_list:
         search = search_playlist(token=token, key=genre, limit=playlist_per_genre)['playlists']['items']
@@ -15,16 +24,30 @@ def build_dataset(genre_list, token, playlist_per_genre=1):
             })
 
     for index in playlist_index:
-        playlist_songs = Playlist(id=index['id'], auth_token=token).get_tracks()[:15]
+        playlist_songs = Playlist(id=index['id'], auth_token=token).get_tracks()
         for song in playlist_songs:
-            song_nm = re.sub("[\(\[].*?[\)\]]", "", song['name'])
-            song_nm = re.sub("[\(\[]\?*?[\)\]]", "", song['name'])
-            song_nm = re.sub("[\(\[]\&*?[\)\]]", "", song['name'])
-            lyrics = azlyrics.lyrics(artist=song['artist'], song=song_nm)
-            print(song_nm)
-            song['lyrics'] = lyrics
-            # if 'Error' not in lyrics:
-            #     song['lyrics'] = lyrics
+
+            audio_analysis = requests.get(spotify_api_base + 'audio-analysis/' + song['id'],
+                                          headers={'Authorization': 'Bearer ' + token})
+
+            song['audio_analysis'] = {
+                'sections': json.loads(audio_analysis.text)['sections'],
+                'track': json.loads(audio_analysis.text)['track']
+            }
+
+            audio_features = requests.get(spotify_api_base + 'audio-features/' + song['id'],
+                                          headers={'Authorization': 'Bearer ' + token})
+
+            song['audio-features'] = json.loads(audio_features.text)
+
+            song['genre'] = index['genre']
+            song['from_playlist'] = {
+                'name': index['name'],
+                'id': index['id']
+            }
+            # print(collection.insert_one(song).inserted_id)
+            criteria = {'id': song['id']}
+            print(collection.update(criteria, song, upsert=True))
         index['tracks'] = playlist_songs
 
     return playlist_index
