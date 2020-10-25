511f4fb86078f080eacc04b9f5e6ffb700c6bb4f
ryanrwillis <ryan.willis@hotmail.ca>
2020-10-24 22:23:00 -0400

    added lyric scraping

--- build_dataset.py
+++ build_dataset.py
@@ -3,6 +3,7 @@ from playlist import Playlist
 from azlyrics import azlyrics
 from config import mongo_connect_uri
 from globals import spotify_api_base
+from urllib.parse import quote
 import pymongo
 import requests
 import json
@@ -26,28 +27,34 @@ def build_dataset(genre_list, token, playlist_per_genre=8):
     for index in playlist_index:
         playlist_songs = Playlist(id=index['id'], auth_token=token).get_tracks()
         for song in playlist_songs:
+            try:
+                audio_analysis = requests.get(spotify_api_base + 'audio-analysis/' + song['id'],
+                                              headers={'Authorization': 'Bearer ' + token})
+                if 'sections' in json.loads(audio_analysis.text):
+                    song['audio_analysis'] = {
+                        'sections': json.loads(audio_analysis.text)['sections'],
+                        'track': json.loads(audio_analysis.text)['track']
+                    }
 
-            audio_analysis = requests.get(spotify_api_base + 'audio-analysis/' + song['id'],
-                                          headers={'Authorization': 'Bearer ' + token})
+                audio_features = requests.get(spotify_api_base + 'audio-features/' + song['id'],
+                                              headers={'Authorization': 'Bearer ' + token})
 
-            song['audio_analysis'] = {
-                'sections': json.loads(audio_analysis.text)['sections'],
-                'track': json.loads(audio_analysis.text)['track']
-            }
+                song['audio-features'] = json.loads(audio_features.text)
+                song['genre'] = index['genre']
+                song['from_playlist'] = {
+                    'name': index['name'],
+                    'id': index['id']
+                }
+                # print(collection.insert_one(song).inserted_id)
+                lyric_request = requests.get(url='https://api.lyrics.ovh/v1/' + quote(song['artist']) + '/' + quote(song['name']))
+                lyrics = json.loads(lyric_request.text)
+                if len(lyrics) is not 0:
+                    song['lyrics'] = lyrics
 
-            audio_features = requests.get(spotify_api_base + 'audio-features/' + song['id'],
-                                          headers={'Authorization': 'Bearer ' + token})
-
-            song['audio-features'] = json.loads(audio_features.text)
-
-            song['genre'] = index['genre']
-            song['from_playlist'] = {
-                'name': index['name'],
-                'id': index['id']
-            }
-            # print(collection.insert_one(song).inserted_id)
-            criteria = {'id': song['id']}
-            print(collection.update(criteria, song, upsert=True))
+                criteria = {'id': song['id']}
+                print(collection.update(criteria, song, upsert=True))
+            except:
+                print('error occurred along the way...')
         index['tracks'] = playlist_songs
 
     return playlist_index
